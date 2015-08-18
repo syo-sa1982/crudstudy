822960a705d861d978004216b85722d8c5d1c99a
syo-sa1982 <fujii1308@gmail.com>
2015-08-18 20:41:48 +0900

    create models

--- db/migrate.go
+++ db/migrate.go
@@ -1,11 +1,13 @@
-package crudstudy
+package main
 
 import (
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/syo-sa1982/crudstudy/models"
 )
 
 func main() {
 	db, _ := gorm.Open("mysql", "root:@/gorm?charset=utf8&parseTime=True")
+	db.CreateTable(&models.User{})
 }
